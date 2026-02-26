dcebcaa5939c36f96d1c104dead17fbc1f091356
SekeyPerino08 <clarkkentperino08@gmail.com>
2026-02-26 20:29:27 +0800

    adjusted the margin and padding of the home page to improve the layout and spacing of the content. Also updated the image dimensions to better fit the design.

--- app/(guess)/(home)/page.tsx
+++ app/(guess)/(home)/page.tsx
@@ -7,17 +7,17 @@ export default function HomePage() {
         <StarsBackground />
       </div>
       <div className="flex items-start gap-12 p-8 text-white">
-        <div className="flex-1">
-          <h1 className="text-4xl font-bold mt-20">Hey, I'm Clark Kent</h1>
-          <p className="text-lg mt-4">
+        <div className="flex-1 mt-40 ml-15">
+          <h1 className="text-4xl font-bold">Hey, I'm Clark Kent</h1>
+          <p className="text-m mt-4">
             I am a web designer and web developer who creates clean, responsive, and user-friendly websites. I combine creativity and technical skills to build modern and efficient web solutions.
           </p>
         </div>
-        <div className="flex-shrink-0 mx-25">
+        <div className="flex-shrink-0 mx-25 mt-12">
           <img 
             src="/images/Gemini_Generated_Image_sta4d4sta4d4sta4.png" 
             alt="Clark Kent" 
-            className="w-120 h-120 object-cover rounded-lg shadow-lg"
+            className="w-120 h-116 object-cover rounded-lg shadow-lg"
           />
         </div>
       </div>
